6a80290e4223e9a900dba03528a87dc424ea5b6f
Jules White <jules.white1@protonmail.com>
2024-10-23 09:23:28 +0000

    update server.zig to use proper endpoints and route methods appropriately

--- src/server.zig
+++ src/server.zig
@@ -7,12 +7,14 @@ const log = @import("stardust").sdlog;
 
 var CONTROLLER: controller = undefined;
 
+var alloc: ?std.mem.Allocator = null;
+
 pub const server = struct {
     port: u32,
 
     pub fn run(self: *const server) !void {
         var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-        const allocator = gpa.allocator();
+        alloc = gpa.allocator();
 
         var listener = zap.HttpListener.init(.{
             .port = self.port,
@@ -21,7 +23,7 @@ pub const server = struct {
 
         // CONTROLLER = try controller.init();
 
-        _ = try song.init(&allocator, 5, 5);
+        _ = try song.init(&alloc.?, 5, 5);
 
         log(@src(), .{ "Listening for incoming connections...", .info });
 
@@ -37,16 +39,61 @@ pub const server = struct {
 
     fn on_request(r: zap.Request) void {
         if (r.path) |path| {
-            if (!std.mem.eql(u8, path, "/api")) {
+            if (alloc) |_| {} else {
+                log(@src(), .{ "Alloc was not initialised", .fatal });
                 return;
+            }
+
+            var it = std.mem.splitAny(u8, path[1..], "/");
+            if (!std.mem.eql(u8, it.first(), "api")) {
+                r.sendBody("please hit /api instead") catch {
+                    log(@src(), .{ "Could not respond to request", .fatal });
+                };
+                return;
+            }
+
+            var endpoints = std.ArrayList([]const u8).init(alloc.?);
+            while (it.next()) |x| {
+                endpoints.append(x) catch {};
+            }
+
+            if (endpoints.items.len == 0 or endpoints.items[0].len == 0) {
+                return;
+            }
+
+            if (std.mem.eql(u8, endpoints.items[0], "tree")) {
+                handle_tree_request(r, endpoints.items);
+            } else if (std.mem.eql(u8, endpoints.items[0], "song")) {
+                handle_song_request(r, endpoints.items);
+            } else if (std.mem.eql(u8, endpoints.items[0], "beat")) {
+                handle_beat_request(r, endpoints.items);
+            } else if (std.mem.eql(u8, endpoints.items[0], "config")) {
+                handle_config_request(r, endpoints.items);
             }
         }
 
         if (r.query) |query| {
             log(@src(), .{ "query:", query, .info });
         }
+    }
 
-        r.sendBody("response") catch {
+    fn handle_tree_request(r: zap.Request, _: [][]const u8) void {
+        r.sendBody("Tree endpoint") catch {
+            log(@src(), .{ "Could not respond to request", .fatal });
+        };
+    }
+    fn handle_song_request(r: zap.Request, _: [][]const u8) void {
+        r.sendBody("Song endpoint") catch {
+            log(@src(), .{ "Could not respond to request", .fatal });
+        };
+    }
+    fn handle_beat_request(r: zap.Request, _: [][]const u8) void {
+        r.sendBody("Beat endpoint") catch {
+            log(@src(), .{ "Could not respond to request", .fatal });
+        };
+    }
+    fn handle_config_request(r: zap.Request, _: [][]const u8) void {
+        r.sendBody("Config endpoint") catch {
             log(@src(), .{ "Could not respond to request", .fatal });
         };
     }
